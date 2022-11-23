cbb19280d39deafd109b7bc8f896343051dcd73a
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2022-11-23 14:01:05 +0100

    Increase test coverage

--- src/main/kotlin/no/nav/arena_tiltak_aktivitet_acl/repositories/PersonSporingDbo.kt
+++ src/main/kotlin/no/nav/arena_tiltak_aktivitet_acl/repositories/PersonSporingDbo.kt
@@ -1,6 +1,6 @@
 package no.nav.arena_tiltak_aktivitet_acl.repositories
 
-class PersonSporingDbo(
+data class PersonSporingDbo(
 	val personIdent: Long,
 	val fodselsnummer: String,
 	val tiltakgjennomforingId: Long) {
