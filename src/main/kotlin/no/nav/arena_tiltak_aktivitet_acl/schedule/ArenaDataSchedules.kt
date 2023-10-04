163149c01287a5a84331a4782dc6b4e6027f8381
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2023-10-04 13:49:41 +0200

    Slutte å slette IGNORED arenadata

--- src/main/kotlin/no/nav/arena_tiltak_aktivitet_acl/schedule/ArenaDataSchedules.kt
+++ src/main/kotlin/no/nav/arena_tiltak_aktivitet_acl/schedule/ArenaDataSchedules.kt
@@ -3,7 +3,6 @@ package no.nav.arena_tiltak_aktivitet_acl.schedule
 import no.nav.arena_tiltak_aktivitet_acl.repositories.ArenaDataRepository
 import no.nav.arena_tiltak_aktivitet_acl.services.RetryArenaMessageProcessorService
 import no.nav.arena_tiltak_aktivitet_acl.utils.AT_MIDNIGHT
-import no.nav.arena_tiltak_aktivitet_acl.utils.ONE_HOUR
 import no.nav.arena_tiltak_aktivitet_acl.utils.ONE_MINUTE
 import no.nav.common.job.JobRunner
 import no.nav.common.job.leader_election.LeaderElectionClient
@@ -34,7 +33,12 @@ open class ArenaDataSchedules(
 		}
 	}
 
-	@Scheduled(fixedDelay = ONE_HOUR, initialDelay = ONE_MINUTE)
+	/* Ignored data kan være nyttig for feilsøking og analyse.
+	Forslår å heller rydde opp manuelt etter en vurdering.
+	Kanskje ting som ble opprettet før aktivitetsplan ble prodsatt kan tas uten problemer.
+	Selv om dette kan bety at vi lagrer data vi strengt tatt ikke skal behandle, så anser jeg dette som et midlertidig behov frem til vellykket migrering.
+	 */
+	//@Scheduled(fixedDelay = ONE_HOUR, initialDelay = ONE_MINUTE)
 	open fun deleteIgnoredArenaData() {
 		if (leaderElectionClient.isLeader) {
 			JobRunner.run("delete_ignored_data") {
