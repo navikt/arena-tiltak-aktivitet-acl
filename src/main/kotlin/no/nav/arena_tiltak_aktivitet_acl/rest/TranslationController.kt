acc8fbb0a8c742a01c146fbfb1fe911a9afaed30
sigurdgroneng <sigurd.groneng@nav.no>
2023-10-31 13:30:47 +0100

    Fix code smells

--- src/main/kotlin/no/nav/arena_tiltak_aktivitet_acl/rest/TranslationController.kt
+++ src/main/kotlin/no/nav/arena_tiltak_aktivitet_acl/rest/TranslationController.kt
@@ -10,16 +10,13 @@ import no.nav.arena_tiltak_aktivitet_acl.auth.AuthService
 import no.nav.arena_tiltak_aktivitet_acl.auth.Issuer
 import no.nav.arena_tiltak_aktivitet_acl.domain.dto.TranslationQuery
 import no.nav.arena_tiltak_aktivitet_acl.domain.kafka.arena.tiltak.DeltakelseId
-import no.nav.arena_tiltak_aktivitet_acl.repositories.AktivitetRepository
 import no.nav.arena_tiltak_aktivitet_acl.services.AktivitetskortIdService
 import no.nav.security.token.support.core.api.Protected
 import no.nav.security.token.support.core.api.ProtectedWithClaims
-import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.server.ResponseStatusException
 import java.util.*
 
 
