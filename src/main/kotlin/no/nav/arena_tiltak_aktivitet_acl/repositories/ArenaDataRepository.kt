11a141ae16b604eb9fcc9030235cbb32ad9c6e69
sigurdgroneng <sigurd.groneng@nav.no>
2024-03-05 09:35:34 +0100

    Cast to text in query

--- src/main/kotlin/no/nav/arena_tiltak_aktivitet_acl/repositories/ArenaDataRepository.kt
+++ src/main/kotlin/no/nav/arena_tiltak_aktivitet_acl/repositories/ArenaDataRepository.kt
@@ -278,7 +278,7 @@ open class ArenaDataRepository(
 		val sql = """
 				SELECT DISTINCT ON (arena_data.arena_id) *
 				FROM arena_data WHERE
-					arena_id = :deltakelseId AND arena_table_name = 'SIAMO.TILTAKDELTAKER'
+					arena_id = :deltakelseId::text AND arena_table_name = 'SIAMO.TILTAKDELTAKER'
 				ORDER BY arena_id, operation_pos;
 		""".trimIndent()
 		return template.queryForObject(sql, mapOf("deltakelseId" to deltakelseArenaId.value), rowMapper)
